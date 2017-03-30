9546de2900c1adc48f524755c0b10684a07a7f70
perqin <perqinxie@gmail.com>
2017-03-30 22:34:21 +0800

    Project template

---
+++ src/main/java/com/bugg/BuGgCoreApplication.java
@@ -0,0 +1,12 @@
+package com.bugg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuGgCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuGgCoreApplication.class, args);
+	}
+}
